f855638892c172236b0092a773a5d05a6f60bca5
Maxim <fok213@mail.ru>
2020-03-22 19:58:25 +0300

    Add files via upload

--- target/classes/sql/import-h2.sql
+++ target/classes/sql/import-h2.sql
@@ -3,7 +3,7 @@ VALUES
   	('Lokesh', 'Gupta', 'Mirana','234235','6346'),
   	('John', 'Doe', 'Valak','282345','76266');
 
-INSERT INTO BILL (Balance, Bank)
+INSERT INTO BILL (Balance, Bank_owner)
 VALUES
   	(10000, 'vtb'),
   	(500, 'sber'),
@@ -24,12 +24,12 @@ VALUES
   	(234, 'sber'),
   	(112632, 'cokoke');
 
-INSERT INTO OPERATION (Type, Amount, terminal_id, account_from_id, account_to_id)
+INSERT INTO OPERATION (Type, Amount, terminal_id, Account_from, Account_to)
 VALUES
   	('Trans', 500,  1, '1', '2'),
   	('Trans', 1500, 1, '2', '3'),
-  	('ADDIn', 1500, 2, '3', '1'),
-  	('Trans', 500,  3, '1', '3');
+  	('ADDIn', 1500, 2, '3', '2'),
+  	('Trans', 500,  3, '3', '1');
 
 -- Users
 -- password in plaintext: "password"
@@ -51,9 +51,11 @@ INSERT INTO ROLE (role_id, role)
 VALUES (2, 'ROLE_USER');
 
 -- User Roles
-INSERT INTO USER_ROLE (user_role_id, user_id, role_id)
-VALUES (1, 1, 1);
-INSERT INTO USER_ROLE (user_role_id, user_id, role_id)
-VALUES (2, 2, 2);
-INSERT INTO USER_ROLE (user_role_id, user_id, role_id)
-VALUES (3, 3, 2);
+INSERT INTO USER_ROLE (user_id, role_id)
+VALUES (1, 1);
+INSERT INTO USER_ROLE (user_id, role_id)
+VALUES (1, 2);
+INSERT INTO USER_ROLE (user_id, role_id)
+VALUES (2, 2);
+INSERT INTO USER_ROLE (user_id, role_id)
+VALUES (3, 2);
